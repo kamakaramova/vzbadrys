0682ecad4778e34fbdf2cc3ff1fa4b0224332125
Камаля Карамова <kamalakaramova@MacBook-Air-Kamala.local>
2026-07-16 11:12:53 +0300

    Обновлён ID виджета Почты России на 62650

--- src/components/PochtaWidget.tsx
+++ src/components/PochtaWidget.tsx
@@ -18,7 +18,7 @@ declare global {
   }
 }
 
-const WIDGET_ID = 62584;
+const WIDGET_ID = 62650;
 const WIDGET_SRC = "https://widget.pochta.ru/map/widget/widget.js";
 
 export default function PochtaWidget({
